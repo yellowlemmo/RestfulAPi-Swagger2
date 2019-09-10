84e0cc8474a2be1d9d329827a6459c21bd47c15d
cuizhichao <czckyd777@163.com>
2019-09-10 16:57:01 +0800

    no message

--- src/main/java/com/restful/demo/restful/main/main.java
+++ src/main/java/com/restful/demo/restful/main/main.java
@@ -58,10 +58,10 @@ public class main {
         return "success";
     }
 
-    @PostMapping(value = "/hello")
-    @ApiOperation(value = "你好人类",notes = "")
-    public String helloUser(){
-
-        return "";
-    }
+//    @PostMapping(value = "/hello")
+//    @ApiOperation(value = "你好人类",notes = "")
+//    public String helloUser(){
+//
+//        return "";
+//    }
 }
